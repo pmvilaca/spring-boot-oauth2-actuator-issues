2610a954fefd7c6d4c970fd624eeb869c6d2967c
Dave Syer <dsyer@pivotal.io>
2016-03-29 17:54:42 +0100

    Update to make it work

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,15 +1,34 @@
 package com.example;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityPrerequisite;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableResourceServer
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+  @Component
+  protected static class ResourceServerConfigurationUpdater implements SecurityPrerequisite {
+    @Autowired
+    private ResourceServerConfiguration resourceServerConfiguration;
+
+    @PostConstruct
+    public void overrideOrderOfResourceServerConfiguration() {
+      resourceServerConfiguration.setOrder(ManagementServerProperties.BASIC_AUTH_ORDER + 1);
     }
+
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
 }
